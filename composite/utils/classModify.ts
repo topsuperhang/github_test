f916209b75e0c2a28d42894c17b70f345cdceaad
topsuperhang <941163782@qq.com>
2021-12-14 19:57:59 +0800

    feat: '给composite模块完善了CN方法，可多参数，多类型传递'

--- composite/utils/classModify.ts
+++ composite/utils/classModify.ts
@@ -1,20 +1,32 @@
 /**
- * 处理参数返回ClassName
- *
+ * 处理目标对象，返回ClassName
  * @param value 目标对象
- *
- * examples: (参数) => (返回值)
- *      ('name') => ('name')
- *      ({name: true, age: false, name: true}) => ('name')
- *      (() => 'name') => ('name')
- *      ({name: true, age: () => false}) => ('name')
+ * @constructor
+ * example：(参数) => (返回值)
+ *      （'name', 'age'） => ('name age')
+ *       ({name: false, age: true, age: true}, 'color') => ('age color')
+ *       ({name: () => false, age: true, age: true}) => ('age')
+ *       (() => 'name') => ('name')
+ *       (() => {name: true, age: true}, 'color') => ('name age color')
  */
-export function CN(value: unknown) {
+export function CN(...args) {
+    let resultStr = args.reduce((pre, cur) => {
+        return addClassName(cur, pre);
+    }, '');
+
+    return resultStr.trim();
+}
+
+/**
+ * 将目标对象与init合并为ClassName串
+ * @param value 目标对象
+ * @param init  之前的ClassName值
+ */
+function addClassName(value: unknown, init: string = '') {
     /*
     * 初始化为一个空格，方便后序去重工作
     * */
-    let classNames: string = ' ';           // 存储类的结果
-
+    let resultStr: string = ' ' + init;           // 存储类的结果
 
     // 参数为普通对象的操作
     if (exactTypeof(value, 'object')) {
@@ -26,31 +38,45 @@ export function CN(value: unknown) {
             }
 
             if (typeof val === 'function') {
-                const rt = CN(val);
+                const rt = addClassName(val);
                 isUse = (typeof rt === 'boolean') ? rt : false;
             }
 
             // 对className进行去重
-            if(isUse && !~classNames.indexOf(' ' + key + ' ')) {
-                classNames += key + ' ';
+            if(isUse && !~resultStr.indexOf(' ' + key + ' ')) {
+                resultStr += key + ' ';
             }
         }
-        return classNames.trim();
+        return resultStr;
     }
 
     // 参数为方法的操作
     if (typeof value === 'function') {
-        return value.call(this);
+        const rt = value.call(this);
+
+        if (typeof rt === 'function' || exactTypeof(rt, 'object') || typeof rt === 'string') {
+            return addClassName(rt, resultStr);
+        }
+
+        return rt;
     }
 
     // 参数为字符串的操作
     if (typeof value === 'string') {
-        return value;
+        let classNames = value.split(' ');
+
+        for(let i = 0, len = classNames.length; i < len; i++) {
+            if (!~resultStr.indexOf(' ' + classNames[i] + ' ')) {
+                resultStr += classNames[i] + ' ';
+            }
+        }
+
+        return resultStr;
     }
 }
 
 /**
- * 判断目标对象是否为期望类型
+ * function: 判断目标对象是否为期望类型
  * @param value 目标对象
  * @param target 期望类型
  */
